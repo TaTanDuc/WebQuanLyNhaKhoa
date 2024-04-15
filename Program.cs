231be8e4de7556019b2bb35b52a2114b2c07cee6
TanDuc <tatanduc2003@gmail.com>
2024-04-15 09:43:24 +0700

    1

--- Program.cs
+++ Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.EntityFrameworkCore;
 using WebQuanLyNhaKhoa.Data;
-using WebQuanLyNhaKhoa.wwwroot.AutoMapper;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -14,8 +13,6 @@ builder.Services.AddDbContext<QlnhaKhoaContext>(options =>
 });
 
 builder.Services.AddScoped<EmailService>();
-
-builder.Services.AddAutoMapper(typeof(AutoMapperProfile));
 
 var app = builder.Build();
 app.UseStaticFiles();
